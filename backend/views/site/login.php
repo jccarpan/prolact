bc61bb6d91e7e59fd18c13c203801f784a761113
jose <jose@jose-PC>
2016-10-03 19:25:40 -0500

    commit: cambiar el aspecto del login

--- backend/views/site/login.php
+++ backend/views/site/login.php
@@ -11,25 +11,35 @@ $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="panel panel-info">
+        <div class="panel-heading">
+            <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Por favor ingrese su usuario y contraseña:</p>
+            <p>Please fill out the following fields to login:</p>
+        </div>
+        <div class="row panel-body">
+            <div class="col-lg-12 center-block">
+                <div class="row">
+                    <div class="col-lg-5">
+                        <?php $form = ActiveForm::begin(['layout' => 'horizontal', 'id' => 'login-form']); ?>
 
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+                        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
 
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+                        <?= $form->field($model, 'password')->passwordInput() ?>
 
-                <?= $form->field($model, 'password')->passwordInput() ?>
+                        <?= $form->field($model, 'rememberMe')->checkbox() ?>
 
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
+                        <div style="color:#999;margin:1em 0">
+                            If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
+                        </div>
 
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                        <div class="form-group">
+                            <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                        </div>
+
+                        <?php ActiveForm::end(); ?>
+                    </div>
                 </div>
-
-            <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
-</div>
